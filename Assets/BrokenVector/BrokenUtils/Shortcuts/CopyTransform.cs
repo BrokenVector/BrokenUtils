ec0ab2eb81b8f255195155405d393ca1e074fe63
Michael Lohr <michidk@live.de>
2016-06-10 14:54:49 +0200

    Added CopyTransform

---
+++ Assets/BrokenVector/BrokenUtils/Shortcuts/CopyTransform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BrokenVector.BrokenUtils.Shortcuts
+{
+    /// <summary>
+    /// Copys the transform values from one transform to another
+    /// </summary>
+    public class CopyTransform : MonoBehaviour
+    {
+
+        public bool Position;
+        public bool Rotation;
+        public bool Size;
+
+        public Transform Source;
+
+        void Update()
+        {
+            if (Position)
+                this.transform.position = Source.position;
+
+            if (Rotation)
+                this.transform.rotation = Source.rotation;
+
+            if (Size)
+                this.transform.localScale = Source.localScale;
+        }
+
+    }
+}
