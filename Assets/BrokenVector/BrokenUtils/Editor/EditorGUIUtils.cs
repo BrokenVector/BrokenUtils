c6b5d2f0a02682b012cb301e990ef4f43d2913e9
Michael Lohr <michidk@live.de>
2016-06-10 14:50:24 +0200

    added EditorGUIUtils & QuaternionUtils

---
+++ Assets/BrokenVector/BrokenUtils/Editor/EditorGUIUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BrokenVector.BrokenUtils.Editor
+{
+	public static class EditorGUIUtils
+	{
+
+	    public static GUIStyle GetEditorStyle(string name)
+	    {
+	        return EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector).GetStyle(name);
+	    }
+
+	}
+}
---
+++ Assets/BrokenVector/BrokenUtils/Utils/QuaternionUtils.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BrokenVector.BrokenUtils.Utils
+{
+    public class QuaternionUtils
+    {
+        public static Quaternion ClampAroundXAxis(Quaternion rotation, float min, float max)
+        {
+            rotation.x /= rotation.w;
+            rotation.y /= rotation.w;
+            rotation.z /= rotation.w;
+            rotation.w = 1;
+
+            float angle = 2f * Mathf.Rad2Deg * Mathf.Atan(rotation.x);
+            angle = Mathf.Clamp(angle, min, max);
+            rotation.x = Mathf.Tan(.5f * Mathf.Deg2Rad * angle);
+
+            return rotation;
+        }
+
+        public static Quaternion ClampAroundYAxis(Quaternion rotation, float min, float max)
+        {
+            rotation.x /= rotation.w;
+            rotation.y /= rotation.w;
+            rotation.z /= rotation.w;
+            rotation.w = 1;
+
+            float angle = 2f * Mathf.Rad2Deg * Mathf.Atan(rotation.y);
+            angle = Mathf.Clamp(angle, min, max);
+            rotation.y = Mathf.Tan(.5f * Mathf.Deg2Rad * angle);
+
+            return rotation;
+        }
+
+        public static Quaternion ClampAroundZAxis(Quaternion rotation, float min, float max)
+        {
+            rotation.x /= rotation.w;
+            rotation.y /= rotation.w;
+            rotation.z /= rotation.w;
+            rotation.w = 1;
+
+            float angle = 2f * Mathf.Rad2Deg * Mathf.Atan(rotation.z);
+            angle = Mathf.Clamp(angle, min, max);
+            rotation.z = Mathf.Tan(.5f * Mathf.Deg2Rad * angle);
+
+            return rotation;
+        }
+    }
+}
