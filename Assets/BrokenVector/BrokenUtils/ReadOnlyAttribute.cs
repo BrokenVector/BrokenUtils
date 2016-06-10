c9729e5f8db475f112354a024fdfa6364fcd90a8
Michael Lohr <michidk@live.de>
2016-06-10 14:45:36 +0200

    added ReadOnlyAttribute

---
+++ Assets/BrokenVector/BrokenUtils/ReadOnlyAttribute.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// from http://answers.unity3d.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+namespace BrokenVector.VoxVR.Utils
+{
+    /// <summary>
+    /// Use this attribute, to mark a value as read only.
+    /// The variable will still be changeable by code, but not in the inspector.
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+#endif
+}
